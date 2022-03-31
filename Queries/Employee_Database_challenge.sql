d22f8a1007d031e8301fd7e1d28459445a5d6f6f
tambam <98443002+tamiespinosa@users.noreply.github.com>
2022-03-31 14:06:55 -0500

    Uploading Deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -28,5 +28,18 @@ FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY title_count DESC;
 
-
+-- Retiring employees titles
+SELECT DISTINCT ON (e.emp_no) 
+	e.emp_no, e.first_name, e.last_name, e.birth_date,
+        de.from_date, de.to_date,
+        ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+INNER JOIN titles AS ti
+ON e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+        AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
 
